e6aa5e50319449cfb099ca734d68e5d9b9c06f0c
이현지 <hyunji0483@naver.com>
2023-11-10 00:25:41 +0900

    Feat: firebase 연동

---
+++ src/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDv9VuDdLwWcfSjgaIF48rkSuyWZGC32Cc",
+  authDomain: "fe-portfolio-1bd36.firebaseapp.com",
+  projectId: "fe-portfolio-1bd36",
+  storageBucket: "fe-portfolio-1bd36.appspot.com",
+  messagingSenderId: "45357718536",
+  appId: "1:45357718536:web:baf536d9d51fafb5e0c5bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
